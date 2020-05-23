df0999f6165a0d26c6c17a52a416c9ea8ccf362e
Martynas <martynasjank@gmail.com>
2020-05-23 12:34:00 +0300

    Removed not used script tags and added few hover effects

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 <html>
 <head>
     <title>Orca Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles/style.css">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
 </head>
 <body>
@@ -40,15 +38,15 @@
 
             <div class="followers mt-auto text-white text-center mb-4">
                 <ul class="d-flex w-100 justify-content-around list-unstyled py-2 px-3">
-                    <li class="w-100">
+                    <li class="w-100 stats">
                         <p class="count font-weight-bold">3479</p>
                         <p class="type font-weight-light">Followers</p>
                     </li>
-                    <li class="w-100">
+                    <li class="w-100 stats">
                         <p class="count font-weight-bold">2345</p>
                         <p class="type font-weight-light">Following</p>
                     </li>
-                    <li class="w-100">
+                    <li class="w-100 stats">
                         <p class="count font-weight-bold">823.765</p>
                         <p class="type font-weight-light">Likes</p>
                     </li>
